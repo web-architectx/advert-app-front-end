23cd843885d7cd32fa695ee627ad0842cf92c7f0
zenge23 <mharietarh@gmai.com>
2024-10-22 16:04:20 +0000

    user homepage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-green'>
-      <div> About Us</div>
-
-      <div>Support</div>
-
-      <div>Our Resources</div>
+    <div className='h-[60vh] bg-lime-300  flex flex-col gap-[1rem]  	'>
+<span className=' flex flex-col  gap-[1rem] '>
+<div>NEED HELP?</div>
+<h1>hHelp Center</h1>
+<div>Contact us</div>
+</span>
+    <div>Support</div>
+    <div>Our Resources</div>
+    <div>Payments methods</div>
     </div>
   )
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,87 @@
 import React from 'react'
 import RootLayout from '../../layouts/RootLayout'
-
+import cars from '../../assets/images/cars.jpg'
+import fashion1 from '../../assets/images/fashion1.jpg'
+import phones from '../../assets/images/phones.jpg'
+import furniture from '../../assets/images/furniture.jpg'
+import agric from '../../assets/images/food.jpg'
+import photo from '../../assets/images/homeappliance.jpg'
+import sports  from '../../assets/images/sports (2).jpg'
+import babies from '../../assets/images/babies.jpg'
+import animals from '../../assets/images/cats.jpg'
+import sneaker from '../../assets/images/sports.jpg'
+import luxury from '../../assets/images/sportcar.jpg'
+import beauty from '../../assets/images/beauty.jpg'
+import homes from '../../assets/images/homes.jpg'
 const Home = () => {
   return (
+    <>
     <RootLayout>
-        <div>
+      <div className='h-[30vh] bg-white flex flex-row gap-[1rem] justify-center mt-[2rem]'>
+        <span>
+        <img src={cars} alt="" className='w-[120px] h-[120px] rounded-[50%]' />
+        <h1>Vehicle</h1>
+        <h4>77 ads</h4>
+        </span>
 
-    </div>
+        <span>
+        <img src={fashion1} alt="" className='w-[120px] h-[120px] rounded-[50%]' />
+<h1>Fashion</h1>
+        </span>
+
+        <span>
+        <img src={phones} alt="" className='w-[120px] h-[120px] rounded-[50%]' />
+<h1>phones & tablets</h1>
+        </span>
+
+
+      <span>
+      <img src={furniture} alt="" className='w-[120px] h-[120px] rounded-[50%]' />
+<h1> Furniture</h1>
+      </span>
+
+      <span>
+      <img src={agric} alt="" className='w-[120px] h-[120px] rounded-[50%]' />
+<h1>Agriculture&Food</h1>
+      </span>
+
+<span>
+<img src={sports} alt="" className='w-[120px] h-[120px] rounded-[50%]' />
+<h1> sports equipments</h1>
+</span>
+
+<span>
+<img src={babies} alt="" className='w-[120px] h-[120px] rounded-[50%]' />
+<h1>babies and kids</h1>
+</span>
+
+<span>
+<img src={animals} alt="" className='w-[120px] h-[120px] rounded-[50%]' />
+<h1>pets</h1>
+</span>
+
+        {/* <img src={cars} alt="" className='w-[200px] h-[200px]' />
+        <img src={cars} alt="" className='w-[200px] h-[200px]' /> */}
+
+      </div>
     </RootLayout>
+
+    <div className='flex flex-row pb-[40px]'>
+      <div className='flex flex-row gap-[1rem] '>
+        <img src={photo}alt=""  className='w-[500px] h-[565px] m-[10px] rounded-[10px] mt-[45px]'/>
+      </div>
+       <div className='gap-[1rem]  grid  grid-cols-2	mt-[3rem] pb-[10px]	rounded-[10px]'>
+        <img src={sneaker} alt="" className='w-[320px] h-[300px] rounded-[10px]' />
+        <img src={luxury} alt="" className='w-[320px] h-[300px] rounded-[10px]' />
+        <img src={beauty} alt="" className='w-[320px] h-[230px] mt-[0.5rem] rounded-[1rem]' />
+        <img src={homes} alt="" className='w-[320px] h-[230px] mt-[0.5rem] rounded-[1rem] ' />
+         
+       </div>
+       <div>
+        {/* <img src= {} alt="" /> */}
+       </div>
+    </div>
+    </>
   )
 }
 
